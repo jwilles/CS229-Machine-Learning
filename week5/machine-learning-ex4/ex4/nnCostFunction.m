58eac310290697f8b33702bfa9fd77e9311bc992
John <johnwilles@gmail.com>
2017-10-18 20:22:17 -0400

    Modify nnCost output

--- week5/machine-learning-ex4/ex4/nnCostFunction.m
+++ week5/machine-learning-ex4/ex4/nnCostFunction.m
@@ -47,9 +47,9 @@ a1 = [ones(size(z1, 1), 1) sigmoid(z1)];
 z2 = a1*Theta2';
 h = sigmoid(z2);
 
-J = ((1/m) * sum(sum((-y_e .* log(h))-((1-y_e) .* log(1-h)))))
+J = ((1/m) * sum(sum((-y_e .* log(h))-((1-y_e) .* log(1-h)))));
 
-J += (lambda/(2*m))*(sum(sum(Theta1(:,2:end).^2))+sum(sum(Theta2(:,2:end).^2)))
+J += (lambda/(2*m))*(sum(sum(Theta1(:,2:end).^2))+sum(sum(Theta2(:,2:end).^2)));
 
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
