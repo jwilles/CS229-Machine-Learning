8b6d606f8b512b270e7999ee9a440261a602863b
John <johnwilles@gmail.com>
2017-11-10 18:53:54 -0500

    Complete Polynomial Feature mapping

--- week6/machine-learning-ex5/ex5/polyFeatures.m
+++ week6/machine-learning-ex5/ex5/polyFeatures.m
@@ -7,16 +7,24 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
+%X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
 %               column of X contains the values of X to the p-th power.
 %
-% 
+%
 
+p
+numel(X)
 
+X_poly = X;
+ 
+for i = 1:p
+  X_poly = [X_poly, X_poly(:,1).^i];
+end 
 
+X_poly = X_poly(:, 2:end)
 
 
 
